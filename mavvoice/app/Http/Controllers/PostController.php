1734e1caa4c345692889d281860a7686525ca7ad
Harish Kommineni <hkommineni@unomaha.edu>
2016-11-22 14:03:57 -0600

    Updated operation is working

--- mavvoice/app/Http/Controllers/PostController.php
+++ mavvoice/app/Http/Controllers/PostController.php
@@ -78,7 +78,10 @@ class PostController extends Controller
      */
     public function edit($id)
     {
-        //
+        //find the post in the database and save it in the database
+        $post = Post::find($id);
+        // return the view and pass in that information
+        return view ('posts.edit')->withPost($post);
     }
 
     /**
@@ -90,7 +93,25 @@ class PostController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        //validate the date
+        $this -> validate($request, array(
+                    'title' => 'required|max:255',
+                    'body' => 'required'
+            ));
+
+        //save the data to database
+        $post = Post::find($id);
+
+        $post->title = $request->input('title');
+        $post->body = $request->input('body');
+
+        $post->save();
+
+        //set flash data with success message
+        Session::flash('success','This post was successfully saved.');
+
+        //redirect page to posts.show
+        return redirect()->route('posts.show',$post->id);
     }
 
     /**
