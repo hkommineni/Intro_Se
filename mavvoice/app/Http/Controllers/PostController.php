0ce77b4e4490963db7accc3d075128e9e6a9667c
Harish Kommineni <hkommineni@unomaha.edu>
2016-11-22 15:00:30 -0600

    Done with CRUD Operations

--- mavvoice/app/Http/Controllers/PostController.php
+++ mavvoice/app/Http/Controllers/PostController.php
@@ -122,6 +122,10 @@ class PostController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::find($id);
+        $post->delete();
+
+        Session::flash('success','Post was successfully deleted');
+        return redirect()->route('posts.index');
     }
 }
