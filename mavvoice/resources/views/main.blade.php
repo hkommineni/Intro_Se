b9edae10eea0c49a0f537d7353f0837d3816e049
Harish Kommineni <hkommineni@unomaha.edu>
2016-11-23 21:28:04 -0600

    Basic authentication

--- mavvoice/resources/views/main.blade.php
+++ mavvoice/resources/views/main.blade.php
@@ -13,6 +13,8 @@
 
       @include('partials._messages')
 
+      {{ Auth::check() ? "Logged In" : "Logged Out" }}
+
       @yield('content')
 
       @include('partials._footer')
--- mavvoice/routes/web.php
+++ mavvoice/routes/web.php
@@ -10,9 +10,26 @@
 | to using a Closure or controller method. Build something great!
 |
 */
+
+// Authentication routes
+Route::get('login', ['as' => 'login', 'uses' => 'Auth\LoginController@showLoginForm']);
+Route::post('login', ['as' => 'login.post', 'uses' => 'Auth\LoginController@login']);
+Route::post('logout', ['as' => 'logout', 'uses' => 'Auth\LoginController@logout']);
+
+
+
+// Registration Routes...
+ Route::get('register', ['as' => 'register', 'uses' => 'Auth\RegisterController@showRegistrationForm']);
+ Route::post('register', ['as' => 'register.post', 'uses' => 'Auth\RegisterController@register']);
+
+
 Route::get('mavoice/{slug}',['as' => 'mavoice.single', 'uses' => 'BlogController@getSingle'])->where('slug', '[\w\d\-\_]+');
 Route::get('mavoice',['as' => 'mavoice.index', 'uses' => 'BlogController@getIndex']);
 Route::get('contact', 'PagesController@getContact');
 Route::get('about', 'PagesController@getabout');
 Route::get('/', 'PagesController@getIndex');
 Route::resource('posts','PostController');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
