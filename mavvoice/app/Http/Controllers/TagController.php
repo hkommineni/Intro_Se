501a0d5c35e313f11dad18e61d6898000ccbf86e
Harish Kommineni <hkommineni@unomaha.edu>
2016-11-24 21:54:33 -0600

    Before starting Comment Section

--- mavvoice/app/Http/Controllers/TagController.php
+++ mavvoice/app/Http/Controllers/TagController.php
@@ -52,7 +52,9 @@ class TagController extends Controller
      */
     public function show($id)
     {
-        //
+        $tag = Tag::find($id);
+        return view('tags.show')->withTag($tag);
+
     }
 
     /**
@@ -63,7 +65,8 @@ class TagController extends Controller
      */
     public function edit($id)
     {
-        //
+        $tag = Tag::find($id);
+        return view('tags.edit')->withTag($tag);
     }
 
     /**
@@ -75,7 +78,13 @@ class TagController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $tag = Tag::find($id);
+        $this->validate($request, ['name' => 'required|max:255']);
+        $tag->name =$request->name;
+        $tag->save();
+
+        Session::flash('success','Tage has been updated successfully!!');
+        return redirect()->route('tags.show', $tag->id);
     }
 
     /**
@@ -86,6 +95,22 @@ class TagController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $tag = Tag::find($id);
+        $tag->posts()->detach();
+
+        $tag->delete();
+
+        Session::flash('success','Tag has been deleted successfully!!');
+
+        return redirect()->route('tags.index');
     }
 }
+
+
+
+
+
+
+
+
+
